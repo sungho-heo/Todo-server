4cf02e758dcaa9428728545eb18435d1938f1445
Sungho <hurgj123kr@gmail.com>
2023-06-09 21:20:56 +0900

    get update

--- src/apiController.js
+++ src/apiController.js
@@ -8,12 +8,10 @@ export const getTodo = async (req, res) => {
 export const postTodo = async (req, res) => {
   const { todoList } = req.body;
   try {
-    if (todoList.length > 2) {
-      const newTodo = new Todo({ todo: todoList });
-      // db에 저장
-      await newTodo.save();
-      res.status(201).json(newTodo);
-    }
+    const newTodo = new Todo({ todo: todoList });
+    // db에 저장
+    await newTodo.save();
+    res.status(201).json(newTodo);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
