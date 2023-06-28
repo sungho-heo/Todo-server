2c4a19d9969983902207e4f57f2914bf4c2ce367
Sungho <hurgj123kr@gmail.com>
2023-06-28 20:24:52 +0900

    cors origin config

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,6 @@ export const postUser = async (req, res) => {
   const user = await User.findOne({ name: req.body.name });
   if (user) {
     req.session.user = user;
-    console.log(req.session.user);
     return res.json(user.name);
   }
   return res.sendStatus(404);
--- src/server.js
+++ src/server.js
@@ -10,7 +10,14 @@ import cors from "cors";
 const app = express();
 
 // CORS error resolve..
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://testtodo-4iip.onrender.com",
+      "https://nimble-crumble-b2341c.netlify.app/",
+    ],
+  })
+);
 app.use(morgan("dev"));
 // body data backend 에서 받기위함
 app.use(express.urlencoded({ extended: true }));
