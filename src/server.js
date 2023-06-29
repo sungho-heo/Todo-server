a2463b0d41b475d0510d635377fb4ff71eaed563
Sungho <hurgj123kr@gmail.com>
2023-06-29 13:21:32 +0900

    cors config update

--- src/server.js
+++ src/server.js
@@ -10,12 +10,7 @@ import cors from "cors";
 const app = express();
 
 // CORS error resolve..
-app.use(
-  cors({
-    credentials: true,
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(morgan("dev"));
 // body data backend 에서 받기위함
 app.use(express.urlencoded({ extended: true }));
