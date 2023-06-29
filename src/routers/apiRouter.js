b2f7ef2eab122b6131cd525698c4fbe891bfc796
Sungho <hurgj123kr@gmail.com>
2023-06-29 16:01:34 +0900

    login user data check middleware create

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -3,8 +3,16 @@ import { postTodo, getTodo, deleteTodo } from "../controllers/apiController.js";
 
 const apiRouter = express.Router();
 
+// 로그인 확인 미들웨어
+const checkAuth = (req, res, next) => {
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+  next();
+};
+
 apiRouter.get("/todo", getTodo);
-apiRouter.post("/todos", postTodo);
+apiRouter.post("/todos", checkAuth, postTodo);
 apiRouter.delete("/todo/delete", deleteTodo);
 
 export default apiRouter;
