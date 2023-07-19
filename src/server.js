efd71358ab8fe9c02f5e2f5fd9dbbeaa8a98e375
Sungho <hurgj123kr@gmail.com>
2023-07-19 13:29:28 +0900

    cors error 3

--- src/server.js
+++ src/server.js
@@ -8,8 +8,12 @@ import userRouter from "./routers/userRouter.js";
 import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: "https://sungho-heo.github.io",
+  optionsSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 // body data backend 에서 받기위함
 app.use(express.urlencoded({ extended: true }));
