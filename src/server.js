69b5bf6b549b37bbf3e1d32a4d6fc4ec9a3d8776
Sungho <hurgj123kr@gmail.com>
2023-07-20 15:02:02 +0900

    url update2

--- src/server.js
+++ src/server.js
@@ -14,7 +14,10 @@ const app = express();
 // };
 // app.use(cors(corsOptions));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://sungho-heo.github.io");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://sungho-heo.github.io/Todo"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
