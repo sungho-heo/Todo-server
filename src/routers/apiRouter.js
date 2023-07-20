d0b25b05b2b48d30d914646ee06c4562cdf7176d
Sungho <hurgj123kr@gmail.com>
2023-07-20 09:37:46 +0900

    router crash error update

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -3,14 +3,6 @@ import { postTodo, getTodo, deleteTodo } from "../controllers/apiController.js";
 
 const apiRouter = express.Router();
 
-// 로그인 확인 미들웨어
-const checkAuth = (req, res, next) => {
-  if (!req.session.user) {
-    return res.sendStatus(401);
-  }
-  next();
-};
-
 apiRouter.get("/todo", getTodo);
 apiRouter.post("/todos", checkAuth, postTodo);
 apiRouter.delete("/todo/delete", deleteTodo);
